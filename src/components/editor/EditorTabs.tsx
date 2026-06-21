ec593106102e599a7a4bd14dc9af912a87324102
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2026-06-20 23:39:12 -0600

    feat: drag and drop for tabs

--- src/components/editor/EditorTabs.tsx
+++ src/components/editor/EditorTabs.tsx
@@ -1,38 +1,46 @@
-import { X } from "lucide-react";
-import { useState } from "react";
 import { useEditor, useEditorDispatch } from "./EditorContext";
 import { Node } from "@/db/types";
+import { useSortable } from "@dnd-kit/react/sortable";
+import { X } from "lucide-react";
+import { useState } from "react";
 
-export default function EditorTabs() {
-	const { nodes, openFiles, selectedFile, unsavedFiles } = useEditor();
+export function EditorTab({ tab, index }: { tab: Node, index: number }) {
+	const { selectedFile } = useEditor();
 	const dispatch = useEditorDispatch();
 
-	const [hovered, setHovered] = useState<Node>();
+	const { ref } = useSortable({ id: tab.id, index, transition: { duration: 0, idle: false, easing: "linear" } });
+
+	const [hovered, setHovered] = useState(false);
+
+	return <div
+		className={`
+			${selectedFile === tab.id ? "bg-ctp-surface0" : ""}
+			hover:bg-ctp-surface0 pl-4 pr-1 py-2 cursor-pointer transition-colors flex gap-1 items-center border-r border-ctp-surface0
+		`}
+		onClick={() => dispatch?.({ type: "select-file", file: tab.id })}
+		onMouseOver={() => setHovered(true)}
+		onMouseLeave={() => setHovered(false)}
+		ref={ref}
+		key={tab.id}
+	>
+		{tab.name}
+		<X
+			className={`size-4 text-ctp-subtext0 hover:bg-ctp-surface1 p-0.5 transition-all
+			${(hovered || selectedFile === tab.id) ? "opacity-100" : "opacity-0"}`}
+			onClick={e => {
+				e.stopPropagation();
+				dispatch?.({ type: "close-file", file: tab.id });
+			}}
+		/>
+	</div>;
+}
+
+export default function EditorTabs() {
+	const { nodes, openFiles } = useEditor();
 
 	return !!openFiles.length ? (
 		<div className="w-full border-b border-ctp-surface0 flex">
-			{openFiles.map(id => nodes.find(f => f.id === id)!).map(tab => (
-				<div
-					className={`
-						${selectedFile === tab.id ? "bg-ctp-surface0" : ""}
-						hover:bg-ctp-surface0 pl-4 pr-1 py-2 cursor-pointer transition-colors flex gap-1 items-center border-r border-ctp-surface0
-					`}
-					onClick={() => dispatch?.({ type: "select-file", file: tab.id })}
-					onMouseOver={() => setHovered(tab)}
-					onMouseLeave={() => setHovered(undefined)}
-					key={tab.id}
-				>
-					{tab.name}
-					{(!unsavedFiles.includes(tab.id) || hovered?.id === tab.id) && <X
-						className={`size-4 text-ctp-subtext0 hover:bg-ctp-surface1 p-0.5 transition-all
-						${(hovered?.id === tab.id || selectedFile === tab.id) ? "opacity-100" : "opacity-0"}`}
-						onClick={e => {
-							e.stopPropagation();
-							dispatch?.({ type: "close-file", file: tab.id });
-						}} />}
-					{(hovered?.id !== tab.id && unsavedFiles.includes(tab.id)) && <div className="rounded-full bg-ctp-text size-1.5 p-0.5 mx-1" />}
-				</div>
-			))}
+			{openFiles.map(id => nodes.find(f => f.id === id)!).map((tab, index) => <EditorTab tab={tab} index={index} key={tab.id} />)}
 		</div>
 	) : null;
 }
