10aeee3eca37a43d914cf504088b54aca80e996b
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2026-08-25 10:31:40 -0600

    feat: error boundary on editor

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -5,6 +5,9 @@ import EditorTabs from "./EditorTabs";
 import Sidebar from "./Sidebar";
 import dynamic from "next/dynamic";
 import EditorTitle from "./EditorTitle";
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
+import Subtext from "../primitives/Subtext";
+import LinkButton from "../primitives/LinkButton";
 
 const MDEditor = dynamic(() => import("@/components/editor/MDEditor"), {
 	ssr: false,
@@ -17,7 +20,12 @@ export default function TabbedEditor() {
 		<div className="w-full h-full flex flex-col">
 			<EditorTabs />
 			<RealmProvider>
-				<MDEditor />
+				<ErrorBoundary errorComponent={props => <>
+					<Subtext>Something went wrong: {(props.error as any).toString()}</Subtext>
+					<LinkButton onClick={props.retry}>Retry</LinkButton>
+				</>}>
+					<MDEditor />
+				</ErrorBoundary>
 			</RealmProvider>
 		</div>
 	</div>);
